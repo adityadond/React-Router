c1475a84274ae093753f1aa814d90ff73b1c5efc
Aditya Dond <63242352+adityadond@users.noreply.github.com>
2023-04-28 17:49:43 +0530

    changes added in profile for toast notification

--- react-router/src/assets/Components/NavBar.jsx
+++ react-router/src/assets/Components/NavBar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "./Auth";
+import { AiFillHome } from "react-icons/ai";
 
 function NavBar() {
     const auth=useAuth()
   return (
     <nav className="navbar">
-      <NavLink to="/">Home</NavLink>
+
+      <NavLink to="/" style={{display: 'flex', alignItems: 'center'}}><AiFillHome size={20} color="black"/>Home</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/products">Products</NavLink>
       <NavLink to="/users">Users</NavLink>
--- react-router/src/assets/Components/Profile.jsx
+++ react-router/src/assets/Components/Profile.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { useAuth } from "./Auth";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const CustomToast=()=>{
+  return (
+    <div>
+      Something went wrong!
+      <button onclick={close}>Close</button>
+    </div>
+  )
+}
 
 function Profile() {
   const auth = useAuth();
@@ -9,10 +20,19 @@ function Profile() {
     auth.logout();
     navigate("/");
   };
+
+  const notify = () => {
+    toast("Basic notify", { position: toast.POSITION.TOP_LEFT, autoClose:8000 });
+    toast.info("Basic notify", { position: toast.POSITION.TOP_CENTER });
+    toast.success("Basic notify", { position: toast.POSITION.TOP_RIGHT, autoClose:false });
+    toast.warn(<CustomToast/>, { position: toast.POSITION.BOTTOM_LEFT });
+    toast.error("Basic notify", { position: toast.POSITION.BOTTOM_CENTER });
+  };
   return (
     <div>
-      Welcome {auth.user}!
-      <button onClick={handleLogout}>Logout</button>
+      Welcome {auth.user}!<button onClick={handleLogout}>Logout</button>
+      <button onClick={notify}>Notify</button>
+      <ToastContainer />
     </div>
   );
 }
